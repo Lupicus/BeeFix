63cbd4fdf7a5ad3c66bf38e285dba90bfa11c52b
Lupicus <54924432+Lupicus@users.noreply.github.com>
2021-11-17 23:15:00 -0600

    Initial code

---
+++ src/main/java/com/lupicus/beefix/Main.java
@@ -0,0 +1,21 @@
+package com.lupicus.beefix;
+
+import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Main.MODID)
+public class Main
+{
+    public static final String MODID = "beefix";
+
+    public Main()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+        ModLoadingContext.get().registerExtensionPoint(DisplayTest.class,
+        		() -> new DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
